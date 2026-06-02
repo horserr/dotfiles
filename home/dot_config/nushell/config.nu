6d3c56e2733442e227f9834ec638ff24ac1bce03
horserr <112157036+horserr@users.noreply.github.com>
2026-06-02 12:47:32 +0800

    completion: add carapace for nushell and pwsh

--- home/dot_config/nushell/config.nu
+++ home/dot_config/nushell/config.nu
@@ -1,15 +1,5 @@
 # config.nu
-#
-# Installed by:
-# version = "0.113.0"
-#
-# This file is used to override default Nushell settings, define
-# (or import) custom commands, or run any other startup tasks.
 # See https://www.nushell.sh/book/configuration.html
-#
-# Nushell sets "sensible defaults" for most configuration settings,
-# so your `config.nu` only needs to override these defaults if desired.
-#
 # You can open this file in your default editor using:
 #     config nu
 #
--- home/dot_config/powershell/completions.ps1
+++ home/dot_config/powershell/completions.ps1
@@ -1,107 +1,110 @@
-# ---------------------------------------------------------
-# 配置与缓存
-# ---------------------------------------------------------
-$Completions = @{
-  "uv"        = @("generate-shell-completion", "powershell")
-  "uvx"       = @("--generate-shell-completion", "powershell")
-  "tailscale" = @("completion", "powershell")
-  "chezmoi" = @("completion", "powershell")
-  "kubectl"   = @("completion", "powershell")
-}
-$modules = @("WSLTabCompletion", "DockerCompletion", "posh-cargo")
+# 将 Tab 键映射为菜单补全
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-$CompletionCacheTtl = [TimeSpan]::FromDays(1)
+# 注入 Carapace 运行时钩子
+carapace _carapace | Out-String | Invoke-Expression
 
-function Update-CompletionCache {
-  param(
-    [string]$CmdName,
-    [string[]]$ArgsList,
-    [bool]$needRefresh = $false
-  )
+# $Completions = @{
+#   "uv"        = @("generate-shell-completion", "powershell")
+#   "uvx"       = @("--generate-shell-completion", "powershell")
+#   "tailscale" = @("completion", "powershell")
+#   "chezmoi" = @("completion", "powershell")
+#   "kubectl"   = @("completion", "powershell")
+# }
+# $modules = @("WSLTabCompletion", "DockerCompletion", "posh-cargo")
 
-  $CacheFile = Join-Path $global:PS_CACHE_ROOT "completion_$CmdName.ps1"
+# $CompletionCacheTtl = [TimeSpan]::FromDays(1)
 
-  $needRefresh = -not (Test-Path $CacheFile)
-  if (-not $needRefresh) {
-    $lastWrite = (Get-Item $CacheFile).LastWriteTimeUtc
-    $expiry = $lastWrite + $CompletionCacheTtl
-    if ((Get-Date).ToUniversalTime() -gt $expiry) {
-      $needRefresh = $true
-    }
-  }
+# function Update-CompletionCache {
+#   param(
+#     [string]$CmdName,
+#     [string[]]$ArgsList,
+#     [bool]$needRefresh = $false
+#   )
 
-  if ($needRefresh -and (Get-Command $CmdName -ErrorAction SilentlyContinue)) {
-    try {
-      & $CmdName @ArgsList | Out-File $CacheFile -Encoding utf8
-    }
-    catch {
-      Write-Error "Failed to refresh completion for ${CmdName}: $_"
-    }
-  }
+#   $CacheFile = Join-Path $global:PS_CACHE_ROOT "completion_$CmdName.ps1"
 
-  return $CacheFile
-}
+#   $needRefresh = -not (Test-Path $CacheFile)
+#   if (-not $needRefresh) {
+#     $lastWrite = (Get-Item $CacheFile).LastWriteTimeUtc
+#     $expiry = $lastWrite + $CompletionCacheTtl
+#     if ((Get-Date).ToUniversalTime() -gt $expiry) {
+#       $needRefresh = $true
+#     }
+#   }
 
-function Import-Completion {
-  param(
-    [string]$CmdName,
-    [string[]]$ArgsList
-  )
+#   if ($needRefresh -and (Get-Command $CmdName -ErrorAction SilentlyContinue)) {
+#     try {
+#       & $CmdName @ArgsList | Out-File $CacheFile -Encoding utf8
+#     }
+#     catch {
+#       Write-Error "Failed to refresh completion for ${CmdName}: $_"
+#     }
+#   }
 
-  $cacheFile = Update-CompletionCache -CmdName $CmdName -ArgsList $ArgsList
-  if (Test-Path $cacheFile) {
-    . $cacheFile
-  }
-}
+#   return $CacheFile
+# }
 
-# 在 Profile 最后添加
-$PostStartTask = {
-  # zoxide
-  $ZoxideCache = "$global:PS_CACHE_ROOT/zoxide_init.ps1"
-  if (!(Test-Path $ZoxideCache)) {
-    if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-      zoxide init powershell | Out-File $ZoxideCache
-    }
-  }
-  if (Test-Path $ZoxideCache) { . $ZoxideCache }
+# function Import-Completion {
+#   param(
+#     [string]$CmdName,
+#     [string[]]$ArgsList
+#   )
 
-  # Winget 补全
-  Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-    $Local:word = $wordToComplete.Replace('"', '""')
-    $Local:ast = $commandAst.ToString().Replace('"', '""')
-    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-      [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    }
-  }
+#   $cacheFile = Update-CompletionCache -CmdName $CmdName -ArgsList $ArgsList
+#   if (Test-Path $cacheFile) {
+#     . $cacheFile
+#   }
+# }
 
-  # USBIPD 补全
-  Register-ArgumentCompleter -Native -CommandName usbipd -ScriptBlock {
-    param($commandName, $wordToComplete, $cursorPosition)
-    usbipd [suggest:$cursorPosition] "$wordToComplete" | ForEach-Object {
-      [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    }
-  }
+# # 在 Profile 最后添加
+# $PostStartTask = {
+#   # zoxide
+#   $ZoxideCache = "$global:PS_CACHE_ROOT/zoxide_init.ps1"
+#   if (!(Test-Path $ZoxideCache)) {
+#     if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+#       zoxide init powershell | Out-File $ZoxideCache
+#     }
+#   }
+#   if (Test-Path $ZoxideCache) { . $ZoxideCache }
 
-  # 1.
-  $modules | ForEach-Object {
-    Import-Module $_
-  }
+#   # Winget 补全
+#   Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+#     param($wordToComplete, $commandAst, $cursorPosition)
+#     [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+#     $Local:word = $wordToComplete.Replace('"', '""')
+#     $Local:ast = $commandAst.ToString().Replace('"', '""')
+#     winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+#       [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+#     }
+#   }
 
-  # 2.
-  foreach ($c in $Completions.Keys) {
-    if (!(Get-Command $c -ErrorAction SilentlyContinue)) {
-      continue
-    }
-    Import-Completion -CmdName $c -ArgsList $Completions[$c]
-  }
+#   # USBIPD 补全
+#   Register-ArgumentCompleter -Native -CommandName usbipd -ScriptBlock {
+#     param($commandName, $wordToComplete, $cursorPosition)
+#     usbipd [suggest:$cursorPosition] "$wordToComplete" | ForEach-Object {
+#       [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+#     }
+#   }
 
-  # 3.
-  Get-ChildItem -Path "$PSScriptRoot/scripts/" -Filter "*_completion.ps1" | ForEach-Object {
-    . $_.FullName
-  }
-}
+#   # 1.
+#   $modules | ForEach-Object {
+#     Import-Module $_
+#   }
 
-# 注册一个引擎事件：在提示符准备就绪后运行
-Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action $PostStartTask | Out-Null
+#   # 2.
+#   foreach ($c in $Completions.Keys) {
+#     if (!(Get-Command $c -ErrorAction SilentlyContinue)) {
+#       continue
+#     }
+#     Import-Completion -CmdName $c -ArgsList $Completions[$c]
+#   }
+
+#   # 3.
+#   Get-ChildItem -Path "$PSScriptRoot/scripts/" -Filter "*_completion.ps1" | ForEach-Object {
+#     . $_.FullName
+#   }
+# }
+
+# # 注册一个引擎事件：在提示符准备就绪后运行
+# Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action $PostStartTask | Out-Null
