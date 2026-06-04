72f7182b6930adbb1abe8ef4cd6e59b0cd315646
horserr <112157036+horserr@users.noreply.github.com>
2026-06-04 20:41:48 +0800

    powershell: remove oh my posh replacing with starship

--- home/dot_config/powershell/env.ps1
+++ home/dot_config/powershell/env.ps1
@@ -22,42 +22,9 @@ $env:FZF_DEFAULT_OPTS = "--height 40% --layout=reverse --border=rounded --previe
 $env:_ZO_FZF_OPTS = "--height 50% --border=rounded --layout=reverse --info=inline"
 
 
-# Oh-My-Posh 初始化（带缓存）
-$themeName = "multiverse-neon"
-# $themeName = "catppuccin_mocha"
-$PoshCache = "$global:PS_CACHE_ROOT/posh_$themeName.ps1"
-if (!(Test-Path $PoshCache)) {
-  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/$themeName.omp.json" | Out-File $PoshCache
-}
-. $PoshCache
-
-# 覆盖 oh-my-posh 自动生成的 prompt 函数
-$oldPrompt = $function:prompt
-function prompt {
-  # 执行原有的 oh-my-posh 逻辑
-  & $oldPrompt
-  # 发送路径信息给 Windows Terminal
-  $loc = $executionContext.SessionState.Path.CurrentLocation
-  if ($loc.Provider.Name -eq "FileSystem") {
-    # 构造 Windows Terminal 识别的 OSC 9;9 序列
-    $path = $loc.ProviderPath
-    Write-Host -NoNewline "$([char]27)]9;9;`"$path`"$([char]27)\"
-  }
-  return " " # 保持格式
-}
-
-
 # Starship 初始化
-# Invoke-Expression (&starship init powershell)
+Invoke-Expression (&starship init powershell)
 # link: https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory#powershell-with-starship
-# function Invoke-Starship-PreCommand {
-#   $loc = $executionContext.SessionState.Path.CurrentLocation;
-#   $prompt = "$([char]27)]9;12$([char]7)"
-#   if ($loc.Provider.Name -eq "FileSystem") {
-#     $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-#   }
-#   $host.ui.Write($prompt)
-# }
 
 ##########################
 
