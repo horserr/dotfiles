959cca63509c5981d7d1943c0ac58ee984ed0de3
horserr <112157036+horserr@users.noreply.github.com>
2026-08-25 21:20:14 +0800

    clash: use global script to add common rules
    Signed-off-by: horserr <112157036+horserr@users.noreply.github.com>

---
+++ home/dot_config/clash-verge-rev/note.md
@@ -0,0 +1,3 @@
+[Guide for clach verge rev](https://clashvergerev.com/guide)
+
+[mihomo, the backend of clash verge rev](https://wiki.metacubex.one/handbook/syntax/)
--- home/dot_config/clash-verge-rev/profiles/Script.js
+++ home/dot_config/clash-verge-rev/profiles/Script.js
@@ -1,5 +1,35 @@
-// Define main function (script entry)
+// reference: https://www.clashverge.dev/guide/script.html
 
-function main(config, profileName) {
+// two types of main entries
+// function main(config) {
+// function main(config, profileName) {
+
+function main(config) {
+  const prependRules = [
+    "PROCESS-NAME,ssh.exe,DIRECT",
+    "PROCESS-NAME,Setup.exe,DIRECT",
+    "PROCESS-NAME,QQMusic.exe,DIRECT",
+    "DOMAIN-SUFFIX,hf.co,DIRECT",
+    "DOMAIN-REGEX,.*mirror[sz]?\..*,DIRECT",
+    "DOMAIN-SUFFIX,mirrorz.org,DIRECT",
+    "DOMAIN-SUFFIX,cr.aliyuncs.com,DIRECT",
+    "DOMAIN-SUFFIX,blob.core.windows.net,DIRECT",
+    "PROCESS-NAME,SDXHelper.exe,DIRECT",
+    "DOMAIN-KEYWORD,nvcr,DIRECT",
+    "DOMAIN,nvidia,DIRECT",
+    "DOMAIN-KEYWORD,daocloud,DIRECT",
+    "DOMAIN-SUFFIX,steamcontent.com,DIRECT",
+    "PROCESS-NAME,qbittorrent.exe,DIRECT",
+    "PROCESS-NAME,WINWORD.EXE,DIRECT",
+    "PROCESS-NAME,rclone.exe,DIRECT",
+    "PROCESS-NAME,yt-dlp.exe,DIRECT",
+    "PROCESS-NAME,Photos.exe,DIRECT",
+    "DOMAIN,cloudconfig.jetbrains.com,DIRECT",
+    "PROCESS-NAME,CrossDeviceService.exe,DIRECT",
+    "PROCESS-NAME,PhoneExperienceHost.exe,DIRECT",
+  ];
+
+  config.rules = [...prependRules, ...config.rules];
+
   return config;
 }
